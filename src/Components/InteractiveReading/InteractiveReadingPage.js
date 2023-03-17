e8f4161c22d2c1929edda90b82cd18d7df29eb83
Mario <liang.chen829@gmail.com>
2023-03-17 19:42:32 +0800

    feat: q2, q3, q4

--- src/Components/InteractiveReading/InteractiveReadingPage.js
+++ src/Components/InteractiveReading/InteractiveReadingPage.js
@@ -5,6 +5,7 @@ import DifficultyLabelAndNext from "../UI/DifficultyLabelAndNext/DifficultyLabel
 import ProgressBar from "../UI/ProgressBar";
 import secondsToMmSs from "../Utils";
 import styles from "./InteractiveReadingPage.module.css";
+import React from "react";
 
 const InteractiveReadingPage = () => {
   const seconds = 420;
@@ -12,7 +13,7 @@ const InteractiveReadingPage = () => {
   const id = searchParams.get("id");
   const [timer, setTimer] = useState(seconds);
   const [showAnswer, setShowAnswer] = useState(false);
-  const index = 0;
+  const [pageIndex, setPageIndex] = useState(0);
 
   const data = require("../../QuestionCollection/阅读/互动阅读/" +
     id +
@@ -45,9 +46,10 @@ const InteractiveReadingPage = () => {
   var indexInLeft = 0;
 
   const onClickNextStepHandler = () => {
-    setShowAnswer(true);
+    setPageIndex((pre) => pre + 1);
   };
 
+  // q1 content
   const content1 = (
     <div className={styles.content}>
       <div className={styles.left}>
@@ -118,6 +120,132 @@ const InteractiveReadingPage = () => {
     </div>
   );
 
+  // q2 content
+  const [middle, setMiddle] = useState("");
+  const [q2Result, setQ2Result] = useState(null);
+
+  const onInputHandler = (e, index) => {
+    setQ2Result(e);
+    setMiddle(e["sentence"]);
+  };
+
+  const onSubmitHandle = () => {
+    setShowAnswer((pre) => !pre);
+  };
+
+  const content2 = (
+    <div className={styles.content}>
+      <div className={styles.left}>
+        <p>{question.q2.front}</p>
+        <div className={styles.middle}>{middle}</div>
+        <p>{question.q2.ending}</p>
+      </div>
+      <div className={styles.right} style={{ flex: 1, marginTop: "20px" }}>
+        {question.q2.options.map((e, index) => {
+          if (showAnswer) {
+            return (
+              <label style={e["correct"] === 1 ? { color: "green" } : {}}>
+                <input
+                  key={index}
+                  type="radio"
+                  name="q2Answer"
+                  value=""
+                  onInput={() => onInputHandler(e, index)}
+                />
+                {e["sentence"]}
+              </label>
+            );
+          } else {
+            return (
+              <label>
+                <input
+                  key={index}
+                  type="radio"
+                  name="q2Answer"
+                  value=""
+                  onInput={() => onInputHandler(e, index)}
+                />
+                {e["sentence"]}
+              </label>
+            );
+          }
+        })}
+      </div>
+    </div>
+  );
+
+  // q3
+  const [selectedText, setSelectedText] = useState("");
+  const handleMouseUp = () => {
+    const selection = window.getSelection().toString();
+    setSelectedText(selection);
+  };
+
+  const content3 = (
+    <div className={styles.content}>
+      <div className={styles.left} onMouseUp={handleMouseUp}>
+        {showAnswer ? (
+          <p
+            dangerouslySetInnerHTML={{
+              __html: question.q3.content.replace(
+                question.q3.selectAnswer,
+                `<span style="background-color: #47b255">${question.q3.selectAnswer}</span>`
+              ),
+            }}
+          />
+        ) : (
+          <p>{question.q3.content}</p>
+        )}
+      </div>
+      <div className={styles.right} style={{ flex: 1, marginTop: "20px" }}>
+        <p>{question.q3.questionContent}</p>
+        <textarea
+          readonly="readonly"
+          autocomplete="off"
+          placeholder="在短文中标出答案"
+          style={{ minHeight: "54px", height: "54px" }}
+          value={selectedText}
+        ></textarea>
+      </div>
+    </div>
+  );
+
+  // q4
+  const [selectedQ4Text, setSelectedQ4Text] = useState("");
+  const handleQ4MouseUp = () => {
+    const selection = window.getSelection().toString();
+    setSelectedQ4Text(selection);
+  };
+
+  const content4 = (
+    <div className={styles.content}>
+      <div className={styles.left} onMouseUp={handleQ4MouseUp}>
+        {showAnswer ? (
+          <p
+            dangerouslySetInnerHTML={{
+              __html: question.q4.content.replace(
+                question.q4.selectAnswer,
+                `<span style="background-color: #47b255">${question.q4.selectAnswer}</span>`
+              ),
+            }}
+          />
+        ) : (
+          <p>{question.q4.content}</p>
+        )}
+      </div>
+      <div className={styles.right} style={{ flex: 1, marginTop: "20px" }}>
+        <p>{question.q4.questionContent}</p>
+        <textarea
+          readonly="readonly"
+          autocomplete="off"
+          placeholder="在短文中标出答案"
+          style={{ minHeight: "54px", height: "54px" }}
+          value={selectedQ4Text}
+        ></textarea>
+      </div>
+    </div>
+  );
+
   return (
     <div className={styles.container}>
       <DifficultyLabelAndNext
@@ -130,8 +258,11 @@ const InteractiveReadingPage = () => {
         <h2 style={{ color: "red" }}>{secondsToMmSs(timer)}</h2>
       </div>
       <ProgressBar progressWidth={progressWidth} />
-      <p>{"#" + id + "-" + (index + 1)}</p>
-      {content1}
+      <p>{"#" + id + "-" + (pageIndex + 1)}</p>
+      {pageIndex === 0 && content1}
+      {pageIndex === 1 && content2}
+      {pageIndex === 2 && content3}
+      {pageIndex === 3 && content4}
       {divider}
       <div
         style={{
@@ -165,6 +296,7 @@ const InteractiveReadingPage = () => {
           下一步
         </button>
       </div>
+      <button onClick={onSubmitHandle}>Submit</button>
     </div>
   );
 };
